125c22793b65578e8c20fd76cba3c6f16e83a0c1
Brijesh <brijeshprajapati2022@gmail.com>
2026-06-15 15:32:27 +0530

    popup project

---
+++ Popup_Card/script.js
@@ -0,0 +1,13 @@
+const popup = document.querySelector('.popup-container');
+const button = document.querySelector('.button');
+const closeIcon = document.querySelector('.close-icon');
+const overlay = document.querySelector('.overlay');
+
+
+button.addEventListener('click', () => {
+    popup.classList.add('popup-open');
+});
+
+closeIcon.addEventListener('click', () => {
+    popup.classList.remove('popup-open');
+})
